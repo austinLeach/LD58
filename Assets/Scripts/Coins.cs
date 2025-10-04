5311cf0dc490f62a3dba2fa78de4d037277606c9
Maxwell300 <schwartzm1231@gmail.com>
2025-10-04 13:06:57 -0500

    Figuring out coin script

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -1,11 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
+
+    private CapsuleCollider2D Collider;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
+        Collider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -13,4 +19,12 @@ public class Coins : MonoBehaviour
     {
        //Check for if player is colliding with coin 
     }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+            Destroy(gameObject);
+            PlayerCoins.CollectCoin();
+    }
+
 }
+
