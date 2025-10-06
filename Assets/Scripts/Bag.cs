ced54b9ab1f73f2664151eef591562c97ba9ae44
Austin Leach <austinleach850@gmail.com>
2025-10-05 23:23:18 -0400

    bag work

--- Assets/Scripts/Bag.cs
+++ Assets/Scripts/Bag.cs
@@ -4,78 +4,41 @@ using UnityEngine.UI;
 
 public class Bag : MonoBehaviour
 {
-
-    public float FirstorSecond;
-    private float pleasehelp;
-    SpriteRenderer sprite;
-    private float test1;
-    private float btnVal;
-    private float maxVal;
-    private float curVal;
+    [SerializeField] SpriteRenderer smallSprite;
+    [SerializeField] SpriteRenderer mediumSprite;
+    [SerializeField] SpriteRenderer largeSprite;
+    private bool medium = false;
+    private bool large = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        pleasehelp = FirstorSecond;
-        sprite = gameObject.GetComponent<SpriteRenderer>();
-        sprite.enabled = false;
-
-        if (FirstorSecond == 0)
-        {
-            pleasehelp = FirstorSecond;
-            FirstorSecond = 1f;
-            sprite.enabled = true;
-        }
-        btnVal = FirstorSecond / 3f;
-
+        smallSprite.enabled = true;
+        mediumSprite.enabled = false;
+        largeSprite.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        maxVal = GlobalVariables.levelCoins;
-        curVal = GlobalVariables.currentCoins;
-        test1 = curVal / maxVal;
+        float maxVal = GlobalVariables.levelCoins;
+        float curVal = GlobalVariables.currentCoins;
+        float currentCoinsPercent = curVal / maxVal;
 
-        //Delete thine self lowly sprite
-        if (pleasehelp == 0)
+        if (!medium && currentCoinsPercent >= 1f / 3f)
         {
-            if (test1 >= 1f / 3f)
-            {
-                Debug.Log("A");
-                //End the life of first bag
-                sprite.enabled = false;
-                Destroy(this);
-                this.enabled = false;
-            }
-        }
-        if (pleasehelp == 1)
-        {
-            if (test1 >= 2f / 3f || test1 == 3f / 3f)
-            {
-                Debug.Log("B");
-                //End the life of second bag
-                sprite.enabled = false;
-                Destroy(this);
-                this.enabled = false;
-            }
+            smallSprite.enabled = false;
+            mediumSprite.enabled = true;
+            largeSprite.enabled = false;
+            medium = true;
         }
 
-
-        if ( test1 >= btnVal)
-        {           
-            if(pleasehelp == 1f)
-            {
-                Debug.Log("C");
-                //Turn on 2nd bag
-                sprite.enabled = true;
-
-            }
-            if (pleasehelp == 2f)
-            {
-                sprite.enabled = true;
-            }
-
+        if (!large && currentCoinsPercent >= 2f / 3f)
+        {
+            smallSprite.enabled = false;
+            mediumSprite.enabled = false;
+            largeSprite.enabled = true;
+            large = true;
         }
 
     }
