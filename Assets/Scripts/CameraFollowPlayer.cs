8e00118583fa4c026921908c3e8357b09f65684e
Austin Leach <austinleach850@gmail.com>
2025-10-06 16:50:14 -0400

    thank you fix

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class Follow_player : MonoBehaviour {
 
     public Transform player;
+    public bool thankYou = false;
 
     // Update is called once per frame
     void Update () {
-        transform.position = player.transform.position + new Vector3(0, 1, -8);
+        if (thankYou)
+        {
+            transform.position = player.transform.position + new Vector3(0, 1, -15);
+            return;
+        }
+        else
+        {
+            transform.position = player.transform.position + new Vector3(0, -1, -8);
+        }
     }
 }
